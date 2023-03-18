a25212d08dc6d8d22f22b17a85ec214a03e3c8d4
RishabhDevSinghVerma <verma.dev.rishabh@gmail.com>
2023-03-18 19:36:26 +0530

    added cpp file

---
+++ Git.cpp
@@ -0,0 +1,20 @@
+                                        ______                    
+                                       /      \    __       __    
+                                       |  ▓▓▓▓▓▓\  |  \     |  \   
+                                       | ▓▓   \▓▓__| ▓▓__ __| ▓▓__ 
+                                       | ▓▓     |    ▓▓  \    ▓▓  \
+                                       | ▓▓   __ \▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+                                       | ▓▓__/  \  | ▓▓     | ▓▓   
+                                        \▓▓    ▓▓   \▓▓      \▓▓   
+                                         \▓▓▓▓▓▓                   
+                            
+ About C++ 
+
+C++ is a high-level programming language created by Danish computer scientist Bjarne Stroustrup. 
+First released in 1985 as an extension of the C programming language.We make fast program using c++.
+We have more control over system resources and memory management.C++ language is a superset of C language.
+
+  C++ Join three seprate programming traditions:-
+  - the procedural language tradition,represented by C;
+  - the Object Oriented language tradition ,represented by the class enhancements C++ adds to C;
+  - Generic programming,supported by C++ templates
