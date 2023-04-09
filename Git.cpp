83f9d7e0693fc7f0ea606b642ae6381db78c723f
RishabhDevSinghVerma <verma.dev.rishabh@gmail.com>
2023-04-09 23:37:56 +0530

    Sum of Array

--- Git.cpp
+++ Git.cpp
@@ -215,3 +215,35 @@ int main()
 
     return 0;
 }
+
+//Code for Sum of Arrays
+
+#include <iostream>
+using namespace std;
+
+int sum(int arr[], int size, int add)
+{
+    for (int i = 0; i < size; i++)
+    {
+        add += arr[i];
+    }
+
+    return add;
+}
+
+int main()
+{
+    int size, add;
+    add = 0;
+    cout << "Enter the Size of Array:";
+    cin >> size;
+    int arr[size];
+    cout << "Enter the Elements of Array:";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << sum(arr, size, add);
+
+    return 0;
+}
